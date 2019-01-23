c5d78c1506f0e1954abbc163622d82eea55ffba8
z1mora <z1mora@ya.ru>
2019-01-23 08:56:07 +0300

    Создал базу для игры "Змейка"

---
+++ Snake_TheGame/Snake_TheGame/Snake_TheGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_TheGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, World");
+            Console.ReadLine();
+        }
+    }
+}
